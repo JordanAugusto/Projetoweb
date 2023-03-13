4a06ec17a59092b41ed142c39529e8bfce4ceb32
Jordan Toledo <96964716+JordanAugusto@users.noreply.github.com>
2023-03-12 23:26:36 -0300

    Update conexaouser.php

--- usina/conexaouser.php
+++ usina/conexaouser.php
@@ -1,9 +1,9 @@
 <?php
 
 $server = "localhost";
-$usuario = "u994259512_cedt";
-$senha = "4+eNKG8ON";
-$banco = "u994259512_crudemail";
+$usuario = "";
+$senha = "";
+$banco = "";
 
 try{
     $conexao = new PDO("mysql:host=$server;dbname=$banco", $usuario,$senha);
